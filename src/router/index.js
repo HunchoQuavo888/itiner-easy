be933e67e0027ca4ce1b6d80da595f0bb8001940
HunchoQuavo888 <ralph.ong.888@gmail.com>
2023-11-09 22:09:17 +0800

    added routing and added authentication to signup

--- src/router/index.js
+++ src/router/index.js
@@ -12,24 +12,21 @@ const router = createRouter({
         { path: "/", component: () => import("../views/home/Home.vue"),meta: {requiresAuth: false}},
         { path: "/sign-up", component: () => import("../views/register/SignUp.vue"),meta: {requiresAuth: false}},
         { path: "/sign-in", component: () => import("../views/login/SignIn.vue"),meta: {requiresAuth: false}},
-        { path: "/feed", component: () => import("../views/Feed.vue"),
-        meta:{
-            requiresAuth:true
-        }},
+        { path: "/feed", component: () => import("../views/Feed.vue"),meta:{requiresAuth:true}},
         //go to converter or translator
-        {path: "/translator", component: () => import("../views/Translator.vue")},
-        {path: "/converter", component: () => import("../views/Converter.vue")},
-        {path: "/tools" , component: () => import("../views/Toolpage.vue")},
+        {path: "/translator", component: () => import("../views/Translator.vue"),meta: {requiresAuth: true}},
+        {path: "/converter", component: () => import("../views/Converter.vue"),meta: {requiresAuth: true}},
+        {path: "/tools" , component: () => import("../views/Toolpage.vue"),meta: {requiresAuth: true}},
 
         //gotobillbuddy.vue
-        {path: "/billbuddy", component: () => import("../views/BillBuddy.vue")},
+        {path: "/billbuddy", component: () => import("../views/BillBuddy.vue"),meta: {requiresAuth: true}},
         {path: "/itinerary", component: () => import("../views/itinerary.vue"),meta: {requiresAuth: true}},
-        {path: "/profile", component: () => import("../views/Profile.vue")},
-        {path: "/add-trip", component: () => import("../views/addtrip.vue")},
-        {path: "/tools", component: () => import ("../views/Toolpage.vue")},
-        {path:"/billbuddytest", component: () => import ("../views/billbuddydraft2.vue")},
-        {path:"/faq", component: () => import ("../views/FAQ.vue")},
-        {path:"/contactus", component:()=> import("../views/contactus.vue")},
+        {path: "/profile", component: () => import("../views/Profile.vue"),meta: {requiresAuth: true}},
+        {path: "/add-trip", component: () => import("../views/addtrip.vue"),meta: {requiresAuth: true}},
+        {path: "/tools", component: () => import ("../views/Toolpage.vue"),meta: {requiresAuth: true}},
+        {path:"/billbuddytest", component: () => import ("../views/billbuddydraft2.vue"),meta: {requiresAuth: true}},
+        {path:"/faq", component: () => import ("../views/FAQ.vue"),meta: {requiresAuth: true}},
+        {path:"/contactus", component:()=> import("../views/contactus.vue"),meta: {requiresAuth: true}},
         { path: '/:catchAll(.*)', component: NotFound, },
         {path: "/itinerary/:tripID", name: 'itinerary-details', component: ItineraryDetails ,meta: {requiresAuth: true}},
         {path: "/profile/:userId", name: 'profile-page', component: ProfilePage ,meta: {requiresAuth: true}},
@@ -50,20 +47,32 @@ const getCurrentUser = () => {
         );
     });
 };
-
-router.beforeEach(async(to, from, next) =>{
+router.beforeEach(async (to, from, next) => {
+    const user = await getCurrentUser();
+    console.log('User:', user); // Log the user object
+  
     if (to.matched.some((record) => record.meta.requiresAuth)) {
-        if (await getCurrentUser()) {
-            next();
+      if (user) {
+        // User is logged in, redirect to /feed if navigating to /
+        if (to.path === '/') {
+          console.log('Redirecting to /feed');
+          next('/feed');
+        } else {
+          console.log('Allowed navigation');
+          next();
         }
-        else {
-            alert ("You don't have access!");
-            next('/');
-        }
+      } else {
+        // User is not logged in, redirect to /
+        console.log('Redirecting to /');
+        next('/');
+      }
+    } else {
+      // Route does not require authentication, proceed
+      console.log('Allowed navigation (no authentication required)');
+      next();
     }
-    else{
-        next();
-    }});
+  });
 
+  
 
 export default router;
